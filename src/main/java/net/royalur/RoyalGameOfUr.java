febdd93093b8aa1ca06ccb1add91da16890a4f48
Padraig Lamont <padraiglamont@gmail.com>
2022-11-14 10:49:44 +0800

    Maven setup

---
+++ src/main/java/net/royalur/RoyalGameOfUr.java
@@ -0,0 +1,12 @@
+package net.royalur;
+
+/**
+ * Hello world!
+ *
+ */
+public class RoyalGameOfUr {
+
+    public static void main( String[] args ){
+        System.out.println("Hello World!");
+    }
+}
