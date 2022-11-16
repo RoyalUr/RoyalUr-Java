8680e2165b02a423b19802a166144347842a11c2
Padraig Lamont <padraiglamont@gmail.com>
2022-11-16 22:45:08 +0800

    Add more Tile tests

--- src/test/java/net/royalur/model/TileTest.java
+++ src/test/java/net/royalur/model/TileTest.java
@@ -20,6 +20,14 @@ public class TileTest {
         assertEquals(25, tile.x);
         assertEquals(999, tile.y);
 
+        for (int x = 0; x < 26; ++x) {
+            for (int y = 0; y < 50; ++y) {
+                tile = new Tile(x, y);
+                assertEquals(x, tile.x);
+                assertEquals(y, tile.y);
+            }
+        }
+
         assertThrows(IllegalArgumentException.class, () -> new Tile(-1, 0));
         assertThrows(IllegalArgumentException.class, () -> new Tile(0, -1));
         assertThrows(IllegalArgumentException.class, () -> new Tile(26, 0));
@@ -29,6 +37,41 @@ public class TileTest {
         assertThrows(IllegalArgumentException.class, () -> new Tile(-5, -4));
         assertThrows(IllegalArgumentException.class, () -> new Tile(35, 4));
         assertThrows(IllegalArgumentException.class, () -> new Tile(35, -9));
+    }
+
+    @Test
+    public void testIsValid() {
+        assertTrue(Tile.isValid(0, 0));
+        assertTrue(Tile.isValid(1, 0));
+        assertTrue(Tile.isValid(0, 1));
+        assertTrue(Tile.isValid(1, 1));
+        assertTrue(Tile.isValid(25, 999));
+        assertTrue(Tile.isValid(12, 12));
+        assertTrue(Tile.isValid(11, 13));
+
+        assertFalse(Tile.isValid(-1, 0));
+        assertFalse(Tile.isValid(1, -1));
+        assertFalse(Tile.isValid(-1, 1));
+        assertFalse(Tile.isValid(1, -1));
+        assertFalse(Tile.isValid(-1, 999));
+        assertFalse(Tile.isValid(12, -1));
+        assertFalse(Tile.isValid(-1, 13));
+
+        assertFalse(Tile.isValid(26, 0));
+        assertFalse(Tile.isValid(1, -1));
+        assertFalse(Tile.isValid(29, 1));
+        assertFalse(Tile.isValid(1, -1));
+        assertFalse(Tile.isValid(34, 999));
+        assertFalse(Tile.isValid(12, -1));
+        assertFalse(Tile.isValid(26, 13));
+
+        for (int x = 0; x < 26; ++x) {
+            assertFalse(Tile.isValid(x, -1));
+            assertFalse(Tile.isValid(x, -5));
+            for (int y = 0; y < 50; ++y) {
+                assertTrue(Tile.isValid(x, y));
+            }
+        }
     }
 
     @Test
@@ -62,5 +105,49 @@ public class TileTest {
         assertEquals(oneZero, new Tile(1, 0));
         assertEquals(zeroOne, new Tile(0, 1));
         assertEquals(oneOne, new Tile(1, 1));
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("A0", new Tile(0, 0).toString());
+        assertEquals("A1", new Tile(0, 1).toString());
+        assertEquals("B0", new Tile(1, 0).toString());
+        assertEquals("B1", new Tile(1, 1).toString());
+        assertEquals("C7", new Tile(2, 7).toString());
+        assertEquals("D15", new Tile(3, 15).toString());
+        assertEquals("F99", new Tile(5, 99).toString());
+        assertEquals("K789", new Tile(10, 789).toString());
+    }
+
+    @Test
+    public void testFromString() {
+        assertEquals(new Tile(0, 0), Tile.fromString("A0"));
+        assertEquals(new Tile(0, 1), Tile.fromString("A1"));
+        assertEquals(new Tile(1, 0), Tile.fromString("B0"));
+        assertEquals(new Tile(1, 1), Tile.fromString("B1"));
+        assertEquals(new Tile(2, 7), Tile.fromString("C7"));
+        assertEquals(new Tile(3, 15), Tile.fromString("D15"));
+        assertEquals(new Tile(5, 99), Tile.fromString("F99"));
+        assertEquals(new Tile(10, 789), Tile.fromString("K789"));
+
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("A"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("B"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("F"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("1"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("7"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString("99"));
+        assertThrows(IllegalArgumentException.class, () -> Tile.fromString(";9"));
+        assertThrows(NumberFormatException.class, () -> Tile.fromString("AA"));
+        assertThrows(NumberFormatException.class, () -> Tile.fromString("AB9"));
+    }
+
+    @Test
+    public void testToFromString() {
+        for (int x = 0; x < 26; ++x) {
+            for (int y = 0; y < 50; ++y) {
+                Tile tile = new Tile(x, y);
+                assertEquals(tile, Tile.fromString(tile.toString()));
+            }
+        }
     }
 }
