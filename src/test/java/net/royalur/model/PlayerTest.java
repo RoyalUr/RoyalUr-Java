bb279d478438ddef1086b378aedaf776df85c7ae
Padraig Lamont <padraiglamont@gmail.com>
2022-11-17 12:51:51 +0800

    Test for Player enum

---
+++ src/test/java/net/royalur/model/PlayerTest.java
@@ -0,0 +1,15 @@
+package net.royalur.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+
+    @Test
+    public void testToCharacter() {
+        assertEquals('.', Player.toChar(null));
+        assertEquals('L', Player.toChar(Player.LIGHT));
+        assertEquals('D', Player.toChar(Player.DARK));
+    }
+}
