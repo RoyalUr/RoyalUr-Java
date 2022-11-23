60c14b745a8a34f1478f962a9be44b4b3f05d720
Padraig Lamont <padraiglamont@gmail.com>
2022-11-23 20:16:58 +0800

    Introduce StandardDice, and remove TetrahedralDice (it is too RoyalUr.net specific)

---
+++ src/main/java/net/royalur/rules/StandardDice.java
@@ -0,0 +1,35 @@
+package net.royalur.rules;
+
+import net.royalur.model.Roll;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+/**
+ * Follows the standard probability distribution for dice
+ * of the Royal Game of Ur, which consist of four D2 die.
+ */
+public class StandardDice implements Dice<Roll> {
+
+    /**
+     * The source of randomness used to generate dice rolls.
+     */
+    private final Random random;
+
+    /**
+     * @param random The source of randomness used to generate dice rolls.
+     */
+    public StandardDice(Random random) {
+        this.random = random;
+    }
+
+    public StandardDice() {
+        this(new Random());
+    }
+
+    @Override
+    public @Nonnull Roll roll() {
+        // Each generated bit represents a roll of a D2 dice.
+        return new Roll(Integer.bitCount(random.nextInt(16)));
+    }
+}
