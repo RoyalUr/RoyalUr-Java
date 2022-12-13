214485e79a718b9e14fc4a60e03069a7f62c1e37
Padraig Lamont <padraiglamont@gmail.com>
2022-12-13 12:20:44 +0800

    Add tests for the random agent playing through standard games, and games of Aseb

---
+++ src/test/java/net/royalur/GameTest.java
@@ -0,0 +1,43 @@
+package net.royalur;
+
+import net.royalur.agent.RandomAgent;
+import net.royalur.model.Player;
+import net.royalur.model.PlayerState;
+import net.royalur.model.Roll;
+import net.royalur.rules.simple.SimplePiece;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameTest {
+
+    @RepeatedTest(3)
+    public void testStandardGameRandom() {
+        Game<SimplePiece, PlayerState, Roll> game = Game.createStandard();
+        RandomAgent<SimplePiece, PlayerState, Roll> light = new RandomAgent<>(game, Player.LIGHT);
+        RandomAgent<SimplePiece, PlayerState, Roll> dark = new RandomAgent<>(game, Player.DARK);
+        int actions = game.playAutonomously(light, dark);
+        assertTrue(game.isFinished());
+
+        // The shortest possible game requires 72 actions.
+        int winMinActions = (2 /* roll + move */) * (4 /* min. moves per piece */) * (7 /* pieces */);
+        int loseMinActions = (2 /* zeroes rolled per piece scored */) * (7 /* pieces */);
+        assertTrue(actions >= winMinActions + loseMinActions);
+        assertTrue(game.states.size() >= actions);
+    }
+
+    @RepeatedTest(3)
+    public void testAsebGameRandom() {
+        Game<SimplePiece, PlayerState, Roll> game = Game.createAseb();
+        RandomAgent<SimplePiece, PlayerState, Roll> light = new RandomAgent<>(game, Player.LIGHT);
+        RandomAgent<SimplePiece, PlayerState, Roll> dark = new RandomAgent<>(game, Player.DARK);
+        int actions = game.playAutonomously(light, dark);
+        assertTrue(game.isFinished());
+
+        // The shortest possible game requires 72 actions.
+        int winMinActions = (2 /* roll + move */) * (4 /* min. moves per piece */) * (7 /* pieces */);
+        int loseMinActions = (2 /* zeroes rolled per piece scored */) * (7 /* pieces */);
+        assertTrue(actions >= winMinActions + loseMinActions);
+        assertTrue(game.states.size() >= actions);
+    }
+}
