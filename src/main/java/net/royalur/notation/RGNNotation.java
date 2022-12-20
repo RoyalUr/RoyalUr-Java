4cdd08016ab93be3b73fbc44c9b7a2b18e38d053
Padraig Lamont <padraiglamont@gmail.com>
2022-12-20 22:43:02 +0800

    Encode metadata for RGN notation

--- src/main/java/net/royalur/notation/RGNNotation.java
+++ src/main/java/net/royalur/notation/RGNNotation.java
@@ -7,11 +7,12 @@ import net.royalur.model.Roll;
 import net.royalur.rules.RuleSet;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 /**
  * RGN stands for Royal Game Notation. This is a textual format
  * that is intended to be readable by both humans and machines.
- * This notation is based upon Chess' PGN (Portable Game Notation).
+ * This notation is inspired by Chess' PGN (Portable Game Notation).
  * @param <P> The type of pieces that are stored on the board.
  * @param <S> The type of state that is stored for each player.
  * @param <R> The type of rolls that may be made.
@@ -31,9 +32,41 @@ public class RGNNotation<P extends Piece, S extends PlayerState, R extends Roll>
         this.rules = rules;
     }
 
+    /**
+     * Escapes {@param value} so that it can be included as a metadata value.
+     * @param value The value to be escaped.
+     * @return The escaped version of {@param value}.
+     */
+    public static @Nonnull String escape(@Nonnull String value) {
+        StringBuilder builder = new StringBuilder("\"");
+        for (char ch : value.toCharArray()) {
+            if (ch == '"') {
+                builder.append("\\\"");
+            } else if (ch == '\\') {
+                builder.append("\\\\");
+            } else {
+                builder.append(ch);
+            }
+        }
+        return builder.append('"').toString();
+    }
+
     @Override
     public @Nonnull String encode(@Nonnull Game<P, S, R> game) {
-        throw new UnsupportedOperationException("TODO");
+        StringBuilder builder = new StringBuilder();
+
+        // Encode the metadata.
+        for (Map.Entry<String, String> item : game.getMetadata().entrySet()) {
+            builder.append("[")
+                    .append(item.getKey())
+                    .append(" ")
+                    .append(escape(item.getValue()))
+                    .append("]\n");
+        }
+        if (builder.length() > 0) {
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 
     @Override
