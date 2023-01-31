3534f563e27439b3eec0b4331ea64e5582d4e1f6
Padraig Lamont <padraiglamont@gmail.com>
2023-01-31 18:56:26 +0800

    Add a deterministic agent for testing

---
+++ src/main/java/net/royalur/agent/DeterministicAgent.java
@@ -0,0 +1,63 @@
+package net.royalur.agent;
+
+import net.royalur.model.*;
+import net.royalur.model.state.WaitingForMoveGameState;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * An agent that makes deterministic move choices for testing. This is not thread-safe.
+ * @param <P> The type of pieces that this agent can interact with.
+ * @param <S> The type of player state that this agent can interact with.
+ * @param <R> The type of rolls that may be made by this agent.
+ */
+public class DeterministicAgent<P extends Piece, S extends PlayerState, R extends Roll> extends Agent<P, S, R> {
+
+    /**
+     * The unique identifier of the first move agent.
+     */
+    public static final @Nonnull String ID = "Deterministic";
+
+    @Override
+    public @Nonnull String getIdentifier() {
+        return ID;
+    }
+
+    @Override
+    public @Nonnull Move<P> decideMove(
+            @Nonnull WaitingForMoveGameState<P, S, R> state,
+            @Nonnull List<Move<P>> moves
+    ) {
+        Move<P> chosen = null;
+        for (Move<P> move : moves) {
+            if (chosen == null) {
+                chosen = move;
+                continue;
+            }
+
+            // Always introduce a piece if it is possible.
+            if (move.isIntroducingPiece()) {
+                chosen = move;
+                break;
+            }
+
+            // Otherwise move the piece that is closest to the top-left.
+            Tile chosenSource = chosen.getSource();
+            int chosenY = chosenSource.y;
+            int chosenX = chosenSource.x;
+
+            Tile moveSource = move.getSource();
+            int moveY = moveSource.y;
+            int moveX = moveSource.x;
+
+            if (moveY < chosenY || (moveY == chosenY && moveX < chosenX)) {
+                chosen = move;
+            }
+        }
+        if (chosen == null)
+            throw new IllegalArgumentException("No moves provided");
+
+        return chosen;
+    }
+}
